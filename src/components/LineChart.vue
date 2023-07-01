3d52aa83027ef57599d2c01cf669c916fb4f7d66
Vickie Chen <vickieq2015@gmail.com>
2023-07-01 12:25:42 -0700

    added all files

---
+++ src/components/LineChart.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+  const options = {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      }
+  const series = [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+
+</script>
+
+<template>
+<div>
+  <h1>Line chart</h1>
+  <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+</div>
+</template>
+
---
+++ src/views/LineChartView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LineChart from '../components/LineChart.vue'
+</script>
+
+<template>
+  <main>
+    <LineChart />
+  </main>
+</template>
+
+  
